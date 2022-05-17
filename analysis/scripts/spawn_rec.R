044318b823e3fb28ca3db2ca5991326135f0b9a8
Mike Ackerman <dirtpathwanderer@gmail.com>
2022-05-17 14:52:31 -0600

    initiated spawner-recruit script

---
+++ analysis/scripts/spawn_rec.R
@@ -0,0 +1,14 @@
+# Author: Mike Ackerman
+#  - using code from Validation_SpawnRec.Rmd in KevinSee/QRFcapacity
+# Purpose: 
+# Created: May 17, 2022
+# Last Modified: 
+# Notes: 
+
+# clear environment
+rm(list = ls())
+
+#-----------------
+# load packages
+library(FSA)
+
